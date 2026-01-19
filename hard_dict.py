e7887f536e8d47dbd997807ddc4973225ce02e3e
ankitbelbase17 <156037828+ankitbelbase17@users.noreply.github.com>
2026-01-20 04:01:08 +0545

    Add HARD_DICT for sampling

---
+++ hard_dict.py
@@ -0,0 +1,33 @@
+HARD_DICT = {
+  "Background/lighting/shadow": {
+    "prob": 0.421875,
+    "items": {
+      "dramatic lighting": 0.500000,
+      "golden hour lighting": 0.500000
+    }
+  },
+  "Camera view": {
+    "prob": 0.112500,
+    "items": {
+      "back view": 0.416700,
+      "angled view": 0.333300,
+      "turned body": 0.250000
+    }
+  },
+  "Occlusion Handling": {
+    "prob": 0.281250,
+    "items": {
+      "multiple overlapping people (heavy occlusion)": 0.400000,
+      "object occluding torso/legs": 0.250000,
+      "face and hands occluded": 0.200000,
+      "folded limbs complex overlaps": 0.150000
+    }
+  },
+  "Special cases": {
+    "prob": 0.184375,
+    "items": {
+      "albinism": 0.600000,
+      "costume with props": 0.400000
+    }
+  }
+}
